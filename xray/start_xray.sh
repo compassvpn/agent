2873da70544e19a33c6f2058ef1096e8f437020e
Ran The <197520740+ranthe21@users.noreply.github.com>
2026-09-02 12:59:46 +0300

    Cap xray Go heap with GOMEMLIMIT (#141)

--- xray/start_xray.sh
+++ xray/start_xray.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+# Go hoards ~2x the live heap without a limit. /proc/meminfo shows host RAM here.
+mem_total_kb=$(awk '/^MemTotal:/ {print $2}' /proc/meminfo)
+case "$mem_total_kb" in
+    ''|*[!0-9]*|0)
+        echo "start_xray: could not read MemTotal from /proc/meminfo (got '$mem_total_kb')" >&2
+        exit 1
+        ;;
+esac
+export GOMEMLIMIT="$(( mem_total_kb * 65 / 100 / 1024 ))MiB"
+echo "start_xray: GOMEMLIMIT=$GOMEMLIMIT (65% of ${mem_total_kb} kB)"
+
 /usr/bin/xray -c /etc/xray/config.json > /var/log/compassvpn/xray.log 2>&1 &
 
 echo $! > /run/xray.pid
